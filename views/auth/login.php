e8caf65214e01026b118b2744d91fc3ea126eb4a
ericktek <erickbale360@gmail.com>
2024-04-13 12:57:56 +0300

    adding favicon

--- views/auth/login.php
+++ views/auth/login.php
@@ -8,27 +8,16 @@ use yii\bootstrap5\ActiveForm;
 use yii\bootstrap5\Html;
 use yii\bootstrap5\Alert;
 
-
-
+$this->title = 'Login';
 ?>
 
-
-
-<div  class="container">
-
-
+<div class="container">
     <div class="row">
-
-
         <div class="col-md-5 offset-md-cust">
             <div class="card">
-  
-
-            <?php $form = ActiveForm::begin([
-                'options' => ['class' => 'card-body cardbody-color p-lg-4'],
-               
-            ]); ?>
-
+                <?php $form = ActiveForm::begin([
+                    'options' => ['class' => 'card-body cardbody-color p-lg-4'],
+                ]); ?>
                 <div class="text-center">
                     <h1 class="title">Sign In</h1>
                     <p class="lead">Online Retailer Point of Sales</p>
@@ -45,15 +34,11 @@ use yii\bootstrap5\Alert;
                 }
                 ?>
 
-
                 <?= $form->field($model, 'username')->textInput(['id' => 'username', 'placeholder' => 'John Doe'])->label('Username') ?>
-
                 <?= $form->field($model, 'password')->passwordInput(['id' => 'password',  'placeholder' => '********'])->label('Password') ?>
-
                 <div class="text-center">
                     <?= Html::submitButton('Login', ['class' => 'btn btn-color px-5 mb-5 w-100']) ?>
                 </div>
-
                 <div class="form-text text-center mb-5 text-dark">
                     Not Registered? <a href="/auth/signup" class="text-dark fw-bold underline-hover">Create an Account</a>
                 </div>
@@ -63,6 +48,3 @@ use yii\bootstrap5\Alert;
         </div>
     </div>
 </div>
-
-
-
--- views/auth/signup.php
+++ views/auth/signup.php
@@ -4,11 +4,10 @@ use yii\helpers\Html;
 
 /** @var yii\web\View $this */
 /** @var app\models\User $model */
+$this->title = 'Register';
 
 ?>
 <div class="user-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
